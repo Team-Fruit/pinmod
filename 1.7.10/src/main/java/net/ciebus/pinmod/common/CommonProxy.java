278e0ada99543e42568c931c4243a36e5ed6cac3
kokoa0429 <kokoa@ciebus.net>
2020-10-07 15:45:09 +0900

    フォルダ移動

---
+++ 1.7.10/src/main/java/net/ciebus/pinmod/common/CommonProxy.java
@@ -0,0 +1,27 @@
+package net.ciebus.pinmod.common;
+
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
+import cpw.mods.fml.common.network.FMLNetworkEvent;
+import net.ciebus.pinmod.Pin;
+import net.ciebus.pinmod.PinManager;
+import net.ciebus.pinmod.common.data.PinData;
+import net.ciebus.pinmod.common.network.PacketHandler;
+import net.ciebus.pinmod.server.PinSynchronizer;
+import net.minecraftforge.common.MinecraftForge;
+
+public class CommonProxy {
+
+    public void onPreInit(FMLPreInitializationEvent event) {
+        PacketHandler.init();
+    }
+
+    public void onInit(FMLInitializationEvent event) {
+        PinSynchronizer.init();
+    }
+
+
+}
